9f3bfeca52be9cf729fd1d4fa4090fae37b433f4
Matt Wilson <matthew.wilson.synesis@gmail.com>
2024-09-11 13:41:55 +1000

    ~ tidying

--- src/lib.rs
+++ src/lib.rs
@@ -1588,7 +1588,7 @@ impl CompiledMatcher {
                             },
                         };
                     },
-                }
+                };
             };
 
             if c == '\n' {
